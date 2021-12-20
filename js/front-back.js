b4515dda4502806a63e284ff1ce0b3270cb70c78
黃禎平 <egg8833@gmail.com>
2021-12-20 18:42:48 +0800

    新增  進版畫面 js

---
+++ js/front-back.js
@@ -0,0 +1,10 @@
+$(function(){
+ 
+    $('.front').click(function(){
+        window.location.href='mainpage.html';
+    });
+    $('.back').click(function(){
+        window.location.href='backweb/backweb.html';
+    });
+   
+});
