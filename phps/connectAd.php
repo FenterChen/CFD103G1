cb163d597c3a1cd32396be87b27e55eff90c9d70
canon810819 <asd310289@gmail.com>
2021-12-23 15:47:04 +0800

    後台管理登入

--- phps/connectAd.php
+++ phps/connectAd.php
@@ -1,7 +1,7 @@
 <?php 
 	$dbname = "tibamefe_cfd103g1";
 	$user = "root";
-	$password = "09Dj4874415";
+	$password = "asd0938551728";
 
 	$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
 
