3c07954483b8de06a735b87c8b8daa9e656cf252
canon810819 <asd310289@gmail.com>
2021-12-30 11:44:12 +0800

    線上求籤更新

--- backweb/phps/connectBOBIO.php
+++ backweb/phps/connectBOBIO.php
@@ -1,7 +1,7 @@
 <?php 
 	$dbname = "tibamefe_cfd103g1";
 	$user = "root";
-	$password = "miumiu0722";
+	$password = "asd0938551728";
 
 	$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
 
